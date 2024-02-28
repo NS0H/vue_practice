d711251111b18b8f3903b65429524ffcc3f53569
NS0H <bory1610@gmail.com>
2024-02-28 18:42:29 +0900

    기술 스택 추가 및 색상 구현

--- src/components/ProfileDisplay.vue
+++ src/components/ProfileDisplay.vue
@@ -1,167 +1,226 @@
 <template>
-    <div class="profile-display">
-        <div class="personal">
-          <div>
-            <div class="name">{{ profile.name }}</div>
-            <button class="re_btn" @click="editProfile">프로필 수정 ></button>
-          </div>
-          <div class="description">{{ profile.des }}</div>
-        </div>
-        <p class="hr"></p>
-        <div>
-            <div class="label">프로필</div>
-            <div class="profile">
-                <div>{{ profile.role }}</div>
-                <div>{{ profile.object }}</div>
-            </div>
-            <div class="label">포트폴리오</div>
-            <div class="result pdf">{{ profile.pdf?.name || '파일 없음' }}</div>
-            <div class="result">{{ profile.link }}</div>
-            <div class="label">기술스택</div>
-            <div class="result">{{ profile.tech }}</div>
-        </div>
+  <div class="profile-display">
+    <!-- 닉네임과 한줄 소개 -->
+    <div class="personal">
+      <div>
+        <div class="name">{{ profile.name }}</div>
+        <button class="re_btn" @click="editProfile">프로필 수정 ></button>
+      </div>
+      <div class="description">{{ profile.des }}</div>
     </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
-  
-  export default defineComponent({
-    name: 'ProfileDisplay',
-    props: {
-      profile: {
-        type: Object as PropType<{ 
-          name: string;
-          des: string;
-          role: string;
-          object: string;
-          pdf: File | null;
-          link: string;
-          tech: string;
-        }>,
-        required: true,
-      },
+    <p class="hr"></p>
+    <!-- 그 외 입력 정보 -->
+    <div>
+      <div class="label">프로필</div>
+      <div class="profile">
+          <div>{{ profile.role || '직무' }}</div>
+          <div>{{ profile.object || '관심분야' }}</div>
+      </div>
+      <div class="label">포트폴리오</div>
+      <div class="result pdf">{{ profile.pdf?.name || '파일 없음' }}</div>
+      <div class="result">{{ profile.link || '링크/URL' }}</div>
+      <div class="label">기술스택</div>
+      <div class="tech_list">
+        <div 
+          class="tech_item" 
+          v-for="(tech, index) in profile.tech" 
+          :key="index"
+          :style="{ backgroundColor: techColors[tech] || 'grey' }">
+          {{ tech }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import techColorsData from '@/assets/techColors.json';
+
+// 기술 스택 별 색상 저장 인터페이스
+interface TechColors {
+  [key: string]: string;
+}
+
+export default defineComponent({
+  name: 'ProfileDisplay',
+  props: {
+    profile: {
+      type: Object as PropType<{ 
+        name: string;
+        des: string;
+        role: string;
+        object: string;
+        pdf: File | null;
+        link: string;
+        tech: string[];
+      }>,
+      required: true,
     },
-    setup(props, { emit }) {
+  },
+  setup(props, { emit }) {
+    // 기술 스택 별 색상 저장
+    const techColors: TechColors = techColorsData;
+
+    // ProfileForm으로 이동
     function editProfile() {
       emit('showForm'); 
     }
 
-    return { editProfile };
+    return { editProfile, techColors };
   }
-  });
-  </script>
+});
+</script>
+
+
+<style lang="scss" scoped>
+.profile-display {
+  display: flex;
+  flex-direction: column;
+
+  width: 360px;
+  padding-top: 48px;
+}
+
+.profile-display > div {
+  padding-left: 20px;
+  width: 320px;
+}
+
+// 닉네임과 프로필 수정 버튼을 감싸는 div
+.personal > div:first-child {
+  display: flex;
+  justify-content: space-between;
+}
+
+// 닉네임
+.name {
+  color: #000;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-weight: 600;
+
+  padding-bottom: 13px;
+}
+
+// 프로필 수정 버튼
+.re_btn {
+  border: none;
+  color: #8B95A1;
+  background-color: #FFFFFF;
+  text-align: right;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.re_btn:hover {
+  cursor: pointer;
+}
+
+// 한줄 소개
+.description {
+  width: 320px;
+  height: 76px;
+  word-wrap: break-word;
   
-  <style lang="scss" scoped>
-  .profile-display {
-    display: flex;
-    flex-direction: column;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 400;
+}
 
-    width: 360px;
-    padding-top: 48px;
-  }
+// 수평선
+.hr {
+  width: 360px;
+  height: 2px;
+  background-color: #F2F4F6;
+}
 
-  .profile-display > div {
-    padding-left: 20px;
-    width: 320px;
-  }
+// 전체 label
+.label {
+  color: #000;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 500;
 
-  .personal > div:first-child {
-    display: flex;
-    justify-content: space-between;
-  }
+  margin-top: 34px;
+  margin-bottom: 18px;
+}
 
-  .name {
-    color: #000;
-    font-family: Pretendard;
-    font-size: 24px;
-    font-weight: 600;
+// 직무 및 관심 분야
+.profile {
+  display: flex;
+  width: 100%;
+  height: 48px;
+  justify-content: space-between;
+}
 
-    padding-bottom: 13px;
-  }
+// 직무와 관심 분야 각각 div
+.profile div {
+  width: 136px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
 
-  .re_btn {
-    border: none;
-    color: #8B95A1;
-    background-color: #FFFFFF;
-    text-align: right;
-    font-family: Pretendard;
-    font-size: 14px;
-    font-weight: 500;
-  }
+  color: #404A5C;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
 
-  .re_btn:hover {
-    cursor: pointer;
-  }
+  border-radius: 6px;
+  border: 1px solid #DADEE2;
+  background: #FFF;
+}
 
-  .description {
-    width: 320px;
-    height: 76px;
-    word-wrap: break-word;
-    
-    color: #000;
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: 400;
-  }
+// 포트폴리오와 링크
+.result {
+  height: 48px;
+  padding-left: 20px;
+  display: flex;
+  align-items: center;
 
-  .hr {
-    width: 360px;
-    height: 2px;
-    background-color: #F2F4F6;
-  }
+  border-radius: 6px;
+  border: 1px solid #DADEE2;
+  background: #FFF;
 
-  .label {
-    color: #000;
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: 500;
+  color: #404A5C;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+}
 
-    margin-top: 34px;
-    margin-bottom: 18px;
-  }
+.pdf {
+  margin-bottom: 12px;
+}
 
-  .profile {
-    display: flex;
-    width: 100%;
-    height: 48px;
-    justify-content: space-between;
-  }
+// 기술 스택
+.tech_list {
+  display: flex;
+  align-items: center;
+  height: auto;
+  flex-wrap: wrap;
+  padding: 0px;
+  column-gap: 7px;
+  row-gap: 12px;
+  max-width: 100%;
 
-  .profile div {
-    width: 136px;
-    padding-left: 20px;
-    display: flex;
-    align-items: center;
+  color: #404A5C;
+  border: none;
+}
 
-    color: #404A5C;
-    font-family: Pretendard;
-    font-size: 14px;
-    font-weight: 500;
+.tech_item {
+  display: inline-flex;
+  padding: 10px 27px;
+  justify-content: center;
+  align-items: center;
 
-    border-radius: 6px;
-    border: 1px solid #DADEE2;
-    background: #FFF;
-  }
-
-  .result {
-    width: 320px;
-    height: 48px;
-    padding-left: 20px;
-    display: flex;
-    align-items: center;
-
-    border-radius: 6px;
-    border: 1px solid #DADEE2;
-    background: #FFF;
-
-    color: #404A5C;
-    font-family: Pretendard;
-    font-size: 14px;
-    font-weight: 500;
-  }
-
-  .pdf {
-    margin-bottom: 12px;
-  }
-  </style>
+  color: #FFF;
+  border-radius: 6px;
+  color: #FFF;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+}
+</style>
--- src/components/ProfileForm.vue
+++ src/components/ProfileForm.vue
@@ -1,74 +1,102 @@
 <template>
-    <div class="profile_form">
-      <form>
-        <div>
-          <label>닉네임</label>
-          <input 
-            v-model="profile.name" 
-            :class="{'highlight': profile.name}"
-            id="name" 
-            type="text">
-            <div v-if="showNameMessage" class="pop_text">
-              사용 가능한 닉네임입니다.
-            </div>
-        </div>
-        <div>
-            <label >한줄소개</label>
-            <textarea 
-              v-model="profile.des"
-              :class="{'highlight': profile.des}" 
-              id="des">
-            </textarea>
-        </div>
-        <div>
-          <label>프로필</label>
-          <div class="profile_select">
-            <select v-model="profile.role" :class="{'highlight': profile.role}" id="role">
-                <option value="" disabled selected>직무</option>
-                <option value="UX Designer">UX Designer</option>
-                <option value="UI Designer">UI Designer</option>
-            </select>
-            <select v-model="profile.object" :class="{'highlight': profile.object}" id="object">
-                <option value="" disabled selected>관심분야</option>
-              <option value="Service Design">서비스 디자인</option>
-              <option value="Web Design">웹 디자인</option>
-            </select>
+  <div class="profile_form">
+    <form>
+      <!-- 이름 입력 -->
+      <div> 
+        <label>닉네임</label>
+        <input 
+          v-model="profile.name" 
+          :class="{'highlight': profile.name}"
+          id="name" 
+          type="text">
+          <div v-if="showNameMessage" class="pop_text">
+            사용 가능한 닉네임입니다.
           </div>
+      </div>
+      <!-- 한줄 소개 -->
+      <div>
+        <label >한줄소개</label>
+        <textarea 
+          v-model="profile.des"
+          :class="{'highlight': profile.des}"
+          id="des">
+        </textarea>
+      </div>
+      <!-- 직무 및 관심 분야 -->
+      <div>
+        <label>프로필</label>
+        <div class="profile_select">
+          <select v-model="profile.role" :class="{'highlight': profile.role}" id="role">
+              <option value="" disabled selected>직무</option>
+              <option value="UX Designer">UX Designer</option>
+              <option value="UI Designer">UI Designer</option>
+          </select>
+          <select v-model="profile.object" :class="{'highlight': profile.object}" id="object">
+              <option value="" disabled selected>관심분야</option>
+            <option value="Service Design">서비스 디자인</option>
+            <option value="Web Design">웹 디자인</option>
+          </select>
         </div>
-        <div class="portfolio">
-            <label>포트폴리오</label>
-            <div class="file_upload"  :class="{ 'highlight': fileName }">
-              <input 
-                :class="{'highlight': profile.pdf}"
-                id="pdf" 
-                type="file" 
-                @change="handleFileChange"
-                hidden>
-              <div class="file_name">{{ fileName ? fileName : '포트폴리오 첨부(PDF 권장)' }}</div>
-              <label for="pdf" class="upload_btn">수정</label>
-            </div>
-              <input 
-                v-model="profile.link" 
-                :class="{'highlight': profile.link}"
-                id="link" 
-                type="text" 
-                placeholder="링크/URL">
-              <div v-if="showLinkMessage" class="pop_text">
-                올바른 형식의 링크를 넣어주세요.
+      </div>
+      <!-- 포트폴리오 및 링크 -->
+      <div class="portfolio">
+        <label>포트폴리오</label>
+        <!-- 파일 업로드 -->
+        <div class="file_upload"  :class="{ 'highlight': fileName }">
+          <input 
+            :class="{'highlight': profile.pdf}"
+            id="pdf" 
+            type="file" 
+            @change="handleFileChange"
+            hidden>
+          <div class="file_name">{{ fileName ? fileName : '포트폴리오 첨부(PDF 권장)' }}</div>
+          <label for="pdf" class="upload_btn">수정</label>
+        </div>
+        <!-- 릴크 첨부 -->
+        <input 
+          v-model="profile.link" 
+          :class="{'highlight': profile.link}"
+          id="link" 
+          type="text" 
+          placeholder="링크/URL">
+        <div v-if="showLinkMessage" class="pop_text">
+          올바른 형식의 링크를 넣어주세요.
+        </div>
+      </div>
+      <!-- 기술 스택 -->
+      <div class="techstack">
+          <label>기술스택</label>
+          <div class="tech_container">
+            <button class="add_tech" type="button" @click="addTech">
+              +
+            </button>
+            <!-- 기술 스택 추가 후 사용되는 div -->
+            <div v-if="profile.tech.length > 0" class="tech_list">
+              <div 
+                class="tech_item" 
+                v-for="(tech, index) in profile.tech" 
+                :key="index"
+                :style="{ backgroundColor: techColors[tech] || 'grey' }">
+                {{ tech }}
               </div>
-        </div>
-        <div class="techstack">
-            <label>기술스택</label>
-            <input v-model="profile.tech" id="tech" type="text" placeholder="기술 스택을 추가해보세요">
-        </div>
-        <button type="button" @click="submitForm">입력 완료</button>
-      </form>
-    </div>
+            </div>
+            <!-- 기술 스택 추가 전 사용되는 div -->
+            <div v-else class="tech_none">
+              기술 스택을 추가해보세요.
+            </div>
+          </div>
+      </div>
+      <button type="button" @click="submitForm">입력 완료</button>
+    </form>
+  </div>
 </template>
-  
+
+
 <script lang="ts">
 import { defineComponent, reactive, computed, ref} from 'vue';
+import techColorsData from '@/assets/techColors.json';
 
+// 입력 값 저장 인터페이스
 interface Profile {
   name: string;
   des: string;
@@ -76,7 +104,12 @@ interface Profile {
   object: string;
   pdf: File | null;
   link: string;
-  tech: string;
+  tech: string[];
+}
+
+// 기술 스택 json값 저장 인터페이스
+interface TechColors {
+  [key: string]: string;
 }
 
 export default defineComponent({
@@ -85,7 +118,8 @@ export default defineComponent({
     formSubmitted: null,
   },
   setup(props, { emit }) {
-    const fileName = ref('');
+    const newTech = ref(''); // 새로 추가된 기술 스택
+    const fileName = ref(''); // 포트폴리오 파일 이름
     const profile = reactive<Profile>({
       name: '',
       des: '',
@@ -93,17 +127,32 @@ export default defineComponent({
       object: '',
       pdf: null,
       link: '',
-      tech: '',
+      tech: [],
     });
 
+    // 기술 스택 별 색상 저장 데이터
+    const techColors: TechColors = techColorsData;
+
+    // 닉네임 확인
     const showNameMessage = computed(() => {
       return !profile.name.includes('a') && profile.name !== '';
     });
 
+    //링크 형식 확인
     const showLinkMessage = computed(() => {
       return !profile.link.includes('http') && !profile.link.includes('www') && profile.link !== '' ;
     });
 
+    // 기술 스택 추가 window prompt
+    const addTech = () => {
+      const newTech = window.prompt("추가할 기술스택을 입력하세요:");
+
+      if (newTech !== null && newTech.trim() !== "") {
+        profile.tech.push(newTech.trim());
+      }
+    };
+
+    // 포트폴리오 파일 추가 함수
     function handleFileChange(event: Event) {
       const inputElement = event.target as HTMLInputElement;
       const files = inputElement.files;
@@ -113,6 +162,7 @@ export default defineComponent({
       }
     }
 
+    // ProfileDisplay로 이동
     function submitForm() {
       emit('formSubmitted', profile);
     }
@@ -120,34 +170,42 @@ export default defineComponent({
     return {
       profile,
       fileName,
-      submitForm,
+      techColors,
+      newTech,
+      addTech,
       showNameMessage,
       showLinkMessage,
-      handleFileChange
+      handleFileChange,
+      submitForm,
     };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-.profile_form {
-    display: flex;
 
-    width: 360px;
-    padding-top: 33px;
+<style lang="scss" scoped>
+// 전체 프로필 폼 스타일
+.profile_form {
+  display: flex;
+
+  width: 360px;
+  padding-top: 33px;
 }
 
 .profile_form form {
-    padding-left: 20px;
-    width: 320px;
+  padding-left: 20px;
+  width: 320px;
 }
+
+// 각 label과 input을 감싸는 div
 form > div {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    margin: 21px auto;
+  margin: 21px auto;
 }
 
+// if문 통해서 나타나는 문구
 .pop_text {
   margin-top: 8px;
 
@@ -157,15 +215,17 @@ form > div {
   font-weight: 500;
 }
 
+// 전체 label
 label {
-    color: #000;
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: 500;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 500;
 
-    margin-bottom: 18px;
+  margin-bottom: 18px;
 }
 
+// 전체적인 input 스타일
 input, select, .file_upload {
     height: 48px;
     border-radius: 6px;
@@ -173,6 +233,7 @@ input, select, .file_upload {
     background: #FFF;
 }
 
+// 한줄 소개
 textarea {
     height: 86px;
     border-radius: 6px;
@@ -182,6 +243,7 @@ textarea {
     resize: none;
 }
 
+// 직무, 관심분야 선택
 .profile_select {
     display: flex;
     width: 100%;
@@ -198,6 +260,7 @@ textarea {
     font-weight: 500;
 }
 
+// 포트폴리오 및 링크
 .portfolio input, .file_name {
     color: #404A5C;
     font-family: Pretendard;
@@ -229,17 +292,69 @@ textarea {
   margin-bottom: 0px;
 }
 
-.techstack input {
-    text-align: center;
-    color: #404A5C;
-    font-family: Pretendard;
-    font-size: 14px;
-    font-weight: 500;
-
-    border: 1px dashed #8B95A1;
+// 기술 스택
+.tech_container {
+  display: flex;
+  justify-content: space-between;
 }
 
-button {
+.add_tech {
+  display: inline-flex;
+  height: 37px;
+  padding: 10px 27px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+
+  border: none;
+  border-radius: 6px;
+  background: #F2F4F6;
+  color: #505967;
+  font-size: 25px;
+  font-weight: 100;
+}
+
+.tech_list, .tech_none {
+  width: 235px;
+  padding: 9px 0px;
+  text-align: center;
+
+  color: #8B95A1;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 6px;
+  border: 1px dashed #8B95A1;
+}
+
+.tech_list {
+  display: flex;
+  align-items: center;
+  height: auto;
+  flex-wrap: wrap;
+  padding: 0px;
+  column-gap: 7px;
+  row-gap: 12px;
+
+  border: none;
+}
+
+.tech_item {
+  display: inline-flex;
+  padding: 10px 27px;
+  justify-content: center;
+  align-items: center;
+
+  color: #FFF;
+  border-radius: 6px;
+  color: #FFF;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-weight: 500;
+}
+
+// 입력 완료 버튼
+form > button {
     width: 320px;
     height: 54px;
     border-radius: 6px;
@@ -255,15 +370,18 @@ button {
     margin-top: 27px;
 }
 
+// 입력 중 스타일
 input:focus, textarea:focus, select:focus, .file_upload:focus {
   border: 1px solid #7A5DF5;
   outline: none;
 }
 
+// 입력 후 스타일
 .highlight {
   border: 1px solid #7A5DF5;
 }
 
+// 버튼 커서 스타일
 button, .upload_btn {
   cursor: pointer;
 }
